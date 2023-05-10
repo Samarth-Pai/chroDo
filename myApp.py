72e110ba3659c08455209f86f70648281fdfeaf8
Samarth Pai <samarthpai9870@gmail.com>
2023-05-10 15:27:23 +0530

    First commit

---
+++ myApp.py
@@ -0,0 +1,123 @@
+from flask import Flask,render_template,request,session,url_for,redirect
+from flask_pymongo import PyMongo,ObjectId
+from flask_session import Session
+import smtplib,random
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/chroDo"
+app.config["SESSION_PERMANENT"] = True
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+db = PyMongo(app).db
+client = db.loginData
+data = db.clientData
+miuiV = 13
+
+def mailer(fromm,passw,to,subject,msg):
+    s = smtplib.SMTP("smtp.office365.com",587)
+    s.starttls()
+    print("Logging in...")
+    s.login(fromm,passw)
+    print("Login successfull.")
+    print("Sending email..")
+    s.sendmail(fromm,to,f"Subject: {subject}\n\n{msg}")
+    print("Mail sent successfully")
+    s.quit()
+
+@app.route("/",methods=["GET","POST"])
+def homePage():
+    global id,todos
+    if request.method=="POST":
+        title=request.form["todoTitle"]
+        desc=request.form["todoDesc"]
+        if title=="":return render_template("nullTitleErr.html",title=title)
+        elif desc=="":return render_template("nullDescErr.html",title=title)
+        else:
+            data.insert_one({"email":session["email"],"title":title,"desc":desc.strip()})
+            todos = [i for i in data.find({"email":session["email"]})]
+            todos.reverse()
+            return render_template("homepage.html",todos=todos,sideTitle=client.find_one({"email":session["email"]})["name"])
+    else:   
+        if session.get("email"):
+            todos = [i for i in data.find({"email":session["email"]})]
+            todos.reverse()
+            return render_template("homepage.html",todos=todos,sideTitle=client.find_one({"email":session["email"]})["name"])
+        else:
+            return render_template("welcomePage.html",sideTitle="chroDo")
+
+@app.route('/login',methods=["GET","POST"])
+def homeLogin():
+    if request.method=="POST":
+        loginUserEmail = request.form["loginUserEmail"]
+        loginUserPassword = request.form["loginUserPassword"]
+        clientSearch = client.find_one({"email":loginUserEmail})
+        if clientSearch is None:
+            return render_template("loginNotFound.html",file="login.html",errorText="Client not found",loginUserEmail=loginUserEmail,loginUserPassword=loginUserPassword,sideTitle="chroDo")
+        elif loginUserPassword!=clientSearch["password"]:
+            return render_template("passwordLenError.html",file="login.html",errorText="Password is incorrect, please try again",loginUserEmail=loginUserEmail,loginUserPassword=loginUserPassword,sideTitle="chroDo")
+        else:
+            session['email']=loginUserEmail
+            return redirect("/")
+    else:
+        return render_template("login.html",sideTitle="chroDo")
+    
+@app.route("/signup",methods=["GET","POST"])
+def homeSignup():
+    global verificationMode,signupPin,userEmail,userName,userPassword
+    if request.method=="POST":
+        if not verificationMode:
+            userEmail = request.form.get("userEmail")
+            userPassword = request.form.get("userPassword")
+            confirmPassword = request.form.get("confirmPassword")
+            userName = request.form.get("userName")
+            if client.find_one({"email":userEmail}) != {}:
+                return render_template("emailExistErr.html",userEmail=userEmail,userPassword=userPassword,confirmPassword=confirmPassword,userName=userName)
+            elif len(userPassword)<8:
+                return render_template("passwordLenError.html",file="signup.html",errorText = "Password length must be at least 8",userEmail=userEmail,userPassword=userPassword,confirmPassword=confirmPassword,userName=userName,sideTitle="chroDo")
+            elif userPassword!=confirmPassword:
+                return render_template("passwordLenError.html",file="signup.html",errorText = "Password does not match with confirmed password",userEmail=userEmail,userPassword=userPassword,confirmPassword=confirmPassword,userName=userName,sideTitle="chroDo")
+            else:
+                verificationMode=True
+                signupPin = random.randrange(100000,999999)
+                mailer("samarthpai9870@hotmail.com","samarth@GM",userEmail,"chroDo signup verification",f"Your OTP for chroDo web application is {signupPin}")
+                return render_template("signupVer.html",userEmail=userEmail)
+        else:
+            if signupPin!=int(request.form.get("userOtp")):
+                return render_template("signupVerErr.html")
+            else:
+                client.insert_one({
+                    "name":userName,
+                    "email":userEmail,
+                    "password":userPassword
+                })
+                session["email"]=userEmail
+                return redirect("/")
+    else:
+        verificationMode= False
+        signupPin=0
+        return render_template("signup.html",userEmail="",userPassword="",confirmPassword="",userName="",sideTitle="chroDo")
+    
+@app.route("/edit_todo/<id>",methods=["GET","POST"])
+def homeEditTodo(id):
+    global todoDesc,todoTitle
+    if request.method=="GET":
+        objectData = data.find_one({"_id":ObjectId(id)})
+        todoTitle,todoDesc=objectData["title"],objectData["desc"]
+        return render_template("editTodo.html",title=todoTitle,desc=todoDesc)
+    else:
+        todoTitle,todoDesc=request.form["todoTitle"],request.form["todoDesc"]
+        done = data.find_one_and_update({"_id":ObjectId(id)},{"$set":{"title":todoTitle,"desc":todoDesc}})
+        # Put something inc before update
+        return redirect("/")
+
+@app.route("/delete_todo/<id>")
+def homeDeleteTodo(id):
+    data.find_one_and_delete({"_id":ObjectId(id)})
+    return redirect("/")
+@app.route("/logout")
+def homeLogout():
+    session["email"]=None
+    return redirect("/")
+
+if __name__ == '__main__':
+    app.run(debug=True)
